0de514a2143450570bb1af77b025fe95eb9880c7
eddybruv <edwinajong@gmail.com>
2022-04-28 18:38:46 +0100

    admin class

---
+++ src/smartCity/Admin.java
@@ -0,0 +1,37 @@
+package smartCity;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Admin {
+
+    static Hotel addHotel() {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Enter new hotel name: ");
+        String name = read.nextLine();
+
+        System.out.print("Enter new hotel town: ");
+        String town = read.nextLine();
+
+        System.out.print("Enter new hotel price: ");
+        int evaluation = read.nextInt();
+
+        System.out.print("Enter new hotel evaluation: ");
+        double price = read.nextInt();
+
+        return new Hotel(name, price, town, evaluation);
+    }
+//    subroutine
+    public static void adminSubroutine(ArrayList<Hotel> hotels) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("How many hotels are to be added?: ");
+        int n = read.nextInt();
+
+        do{
+            Hotel newHotel = Admin.addHotel();
+            hotels.add(newHotel);
+            n -= 1;
+        }while(n > 0);
+    }
+}
