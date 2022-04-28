24911583d0b3a8d286d0d5ba663b3ac55e1a8969
eddybruv <edwinajong@gmail.com>
2022-04-28 18:40:34 +0100

    user class

---
+++ src/smartCity/User.java
@@ -0,0 +1,53 @@
+package smartCity;
+
+import java.util.Scanner;
+
+public class User {
+    static public void userSubroutine () {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Choices \n1. Search by evaluation.\n2. Search by town. \n3. Make recommendation \n4. Quit" );
+        int choice = read.nextInt();
+        do {
+            if (choice == 1) {
+                System.out.print("Enter evaluation to search: ");
+                int evaluation = read.nextInt();
+
+                for (int n = 0; n < Main.registeredHotels.size(); n++) {
+                    if (Main.registeredHotels.get(n).evaluation >= evaluation) {
+                        System.out.println("Available Hotels: \n");
+                        System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name + "\n" + "Evaluation: " + Main.registeredHotels.get(n).evaluation);
+                    }
+                }
+            } else if (choice == 2) {
+                System.out.print("Enter evaluation to search: ");
+                String town = read.nextLine();
+                int count = 0;
+                for (int n = 0; n < Main.registeredHotels.size(); n++) {
+
+                    if (Main.registeredHotels.get(n).town.equals(town)) {
+                        count++;
+                        System.out.println("Available Hotels: \n");
+                        System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name + "\n" + "Town: " + Main.registeredHotels.get(n).town);
+                    }
+                }
+                if (count == 0) {
+                    System.out.println("No results found");
+                }
+            } else if (choice == 3) {
+                for(int n = 0; n < Main.registeredHotels.size(); n++) {
+                    System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name);
+                }
+                System.out.print("Enter hotel to recommend: " );
+                String name = read.nextLine();
+                for(int n = 0; n < Main.registeredHotels.size(); n++) {
+                    if(Main.registeredHotels.get(n).name.equals(name))
+                        Main.registeredHotels.get(n);
+                }
+            }
+            else if (choice == 4) {
+                break;
+            }
+        }while (true);
+    }
+}
