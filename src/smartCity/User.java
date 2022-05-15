2d0e16264282ba49316673e8c56893a8ec6387c1
eddybruv <edwinajong@gmail.com>
2022-05-15 15:21:46 +0100

    added more user functionalities like search

--- src/smartCity/User.java
+++ src/smartCity/User.java
@@ -6,38 +6,54 @@ public class User {
     static public void userSubroutine () {
         Scanner read = new Scanner(System.in);
 
-        System.out.print("User Choices \n1. Search by evaluation.\n2. Search by town. \n3. Make recommendation \n4. Quit\n" );
-        int choice = read.nextInt();
+
         do {
+            System.out.print("User Choices\n~~~~~~~~~~~~~~~\n1. Search by evaluation.\n2. Search by town. \n3. Make recommendation \n4. Return to Menu\n" );
+            int choice = read.nextInt();
+
+//            Search by evaluation / stars
             if (choice == 1) {
+                int count = 0;
                 System.out.print("Enter evaluation to search: ");
                 int evaluation = read.nextInt();
 
+                System.out.println("Available Hotels above "+evaluation+":\n");
                 for (int n = 0; n < Main.registeredHotels.size(); n++) {
                     if (Main.registeredHotels.get(n).evaluation >= evaluation) {
-                        System.out.println("Available Hotels: \n");
                         System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name + "\n" + "Evaluation: " + Main.registeredHotels.get(n).evaluation);
+                        count++;
                     }
+                    System.out.println("\n");
                 }
-            } else if (choice == 2) {
-                System.out.print("Enter evaluation to search: ");
-                String town = read.nextLine();
+
+                if (count == 0) {
+                    System.out.println("No hotels found!\nTry a lower evaluation :)\n");
+                }
+
+            }
+
+//            Search by town
+            else if (choice == 2) {
+                System.out.print("Enter town of search: \n");
+                String town = read.next();
+
                 int count = 0;
+                System.out.println("Available Hotels in "+town+":\n");
+
                 for (int n = 0; n < Main.registeredHotels.size(); n++) {
 
                     if (Main.registeredHotels.get(n).town.equals(town)) {
                         count++;
-                        System.out.println("Available Hotels: \n");
                         System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name + "\n" + "Town: " + Main.registeredHotels.get(n).town);
                     }
                 }
                 if (count == 0) {
-                    System.out.println("No results found");
+                    System.out.println("No results found!\nTry a different town :)\n");
                 }
-            } else if (choice == 3) {
-                for(int n = 0; n < Main.registeredHotels.size(); n++) {
-                    System.out.println(n + 1 + ". " + Main.registeredHotels.get(n).name);
-                }
+            }
+
+//          Recommend Hotel
+            else if (choice == 3) {
                 System.out.print("Enter hotel to recommend: " );
                 String name = read.nextLine();
                 for(int n = 0; n < Main.registeredHotels.size(); n++) {
